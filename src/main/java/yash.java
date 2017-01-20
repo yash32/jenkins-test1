de82f471ddd6f3f6224f5d54c598fcd93ea7ed60
mouli <mouli>
2017-01-19 20:51:56 -0500

    jenkins-test1 pom

---
+++ src/main/java/yash.java
@@ -0,0 +1,15 @@
+public class yash{
+ 
+     public static void main (String args[]){
+ 
+     int a,b,c;
+ 
+     a = 10;
+     b = 20;
+     c = a+b;
+ 
+     System.out.println(""+c);
+ 
+   }
+ 
+}
